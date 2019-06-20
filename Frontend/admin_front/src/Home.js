9e33583aa7784d6961b3d276b18d39e93a21960a
jaseyrae9 <jelena.surlan9@uns.ac.rs>
2019-06-20 12:51:04 +0200

    Ispravka.

--- Frontend/admin_front/src/Home.js
+++ Frontend/admin_front/src/Home.js
@@ -1,55 +1,34 @@
 import React from 'react'
-import { Table } from 'reactstrap'
-import { Link } from 'react-router-dom'
+import { Nav, NavItem } from 'reactstrap'
+import { NavLink } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <Table dark>
-            <tbody>
-                <tr>
-                    <th scope="row" className="col-1 text-center">1</th>
-                    <td>
-                        <Link to="/types">Room types</Link>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row" className="col-1 text-center">2</th>
-                    <td>
-                        <Link to="/categories">Room category</Link>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row" className="col-1 text-center">3</th>
-                    <td>
-                        <Link to="/additional_services">Additional services</Link>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row" className="col-1 text-center">5</th>
-                    <td>
-                        <Link to="/hotels">Hotels</Link>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row" className="col-1 text-center">5</th>
-                    <td>
-                        <Link to="/customers">Customers</Link>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row" className="col-1 text-center">4</th>
-                    <td>
-                        <Link to="/add_agent">New agent</Link>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row" className="col-1 text-center">6</th>
-                    <td>
-                        <Link to="/recensions">Recension</Link>
-                    </td>
-                </tr>
-            </tbody>
-        </Table>
+        <div className = "tema">
+        <Nav vertical className="p-3">
+          <NavItem  className="link">
+            <NavLink to="/types">Room types</NavLink>
+          </NavItem>
+          <NavItem  className="link">
+            <NavLink to="/categories">Room category</NavLink>
+          </NavItem>
+          <NavItem  className="link">
+            <NavLink to="/additional_services">Additional services</NavLink>
+          </NavItem>
+          <NavItem  className="link">
+            <NavLink to="/hotels">Hotels</NavLink>
+          </NavItem>
+          <NavItem  className="link">
+            <NavLink to="/customers">Customers</NavLink>
+          </NavItem>
+          <NavItem  className="link">
+            <NavLink to="/add_agent">New agent</NavLink>
+          </NavItem>
+          <NavItem  className="link">
+            <NavLink to="/recensions">Recensions</NavLink>
+          </NavItem>
+        </Nav>
+      </div>
     )
 }
 
