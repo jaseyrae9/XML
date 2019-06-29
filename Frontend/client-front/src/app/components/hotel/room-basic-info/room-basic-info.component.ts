6fa3b63980b0c720c5ff39df577199c74569461c
jaseyrae9 <jelena.surlan9@uns.ac.rs>
2019-06-29 22:57:33 +0200

    Svasta.

--- Frontend/client-front/src/app/components/hotel/room-basic-info/room-basic-info.component.ts
+++ Frontend/client-front/src/app/components/hotel/room-basic-info/room-basic-info.component.ts
@@ -9,9 +9,17 @@ import { RoomPreview } from 'src/app/model/room/roomPreview';
 export class RoomBasicInfoComponent implements OnInit {
   @Input() room: RoomPreview;
 
+  url = 'assets/images/room/queen/medium.jpg';
+
   constructor() { }
 
   ngOnInit() {
+    const uints = new Uint8Array(this.room.mainImage);
+    const stringChar = String.fromCharCode.apply(null, uints);
+
+    const base64 = btoa(String.fromCharCode(stringChar));
+    this.url = 'data:image/jpeg;base64,' + base64; // use t
+    console.log(this.url);
   }
 
 }
